8099caaa99c88ccdde15dc66cd308b397c3ea6dc
Piepongwong <j.tonneyck@gmail.com>
2019-10-06 17:59:26 +0200

    minor retrigger change

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ app.use((req,res, next)=> {
 })
 
 app.use(function (err, req, res, next) {
-    console.log(err)
     if(err)res.status(err.status).json({message: err.message})
     else res.status(500).json({message: "Oeeeps, something went wrong."})
 })
